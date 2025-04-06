d1e94a64864d7efa9200b9108f6100bb123e8934
Thomas Fisher <tfisher013@gmail.com>
2025-04-05 22:28:45 -0600

    Set up dynamic piece placement

--- Assets/Python/CvCiv4ChessEventManager.py
+++ Assets/Python/CvCiv4ChessEventManager.py
@@ -19,6 +19,69 @@ import os
 gc = CyGlobalContext()
 localText = CyTranslator()
 
+BOARD_LENGTH = 8
+
+PIECE_SETS = {
+	"ANCIENT": {
+		"PAWN": gc.getInfoTypeForString("UNIT_WARRIOR"),
+		"ROOK": gc.getInfoTypeForString("UNIT_AXEMAN"),
+		"KNIGHT": gc.getInfoTypeForString("UNIT_CHARIOT"),
+		"BISHOP": gc.getInfoTypeForString("UNIT_ARCHER"),
+		"QUEEN": gc.getInfoTypeForString("UNIT_SPY"),
+		"KING": gc.getInfoTypeForString("UNIT_PROPHET")
+	}
+}
+
+def setupPieces(pieceSet="ANCIENT"):
+	""" Sets up the pieces on the board
+	"""
+
+	# set up white pieces
+	iActivePlayer = 1
+
+	# pawns
+	for iX in range(1, 9):
+		gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["PAWN"], iX, 7, UnitAITypes.UNITAI_ATTACK)
+	
+	# rooks
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["ROOK"], 1, 8, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["ROOK"], 8, 8, UnitAITypes.UNITAI_ATTACK)
+
+	# knights
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["KNIGHT"], 2, 8, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["KNIGHT"], 7, 8, UnitAITypes.UNITAI_ATTACK)
+
+	# bishops
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["BISHOP"], 3, 8, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["BISHOP"], 6, 8, UnitAITypes.UNITAI_ATTACK)
+
+	# king and queen
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["QUEEN"], 5, 8, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["KING"], 4, 8, UnitAITypes.UNITAI_ATTACK)
+
+	# set up black pieces
+	iActivePlayer = 0
+
+	# pawns
+	for iX in range(1, 9):
+		gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["PAWN"], iX, 2, UnitAITypes.UNITAI_ATTACK)
+	
+	# rooks
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["ROOK"], 1, 1, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["ROOK"], 8, 1, UnitAITypes.UNITAI_ATTACK)
+
+	# knights
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["KNIGHT"], 2, 1, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["KNIGHT"], 7, 1, UnitAITypes.UNITAI_ATTACK)
+	# bishops
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["BISHOP"], 3, 1, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["BISHOP"], 6, 1, UnitAITypes.UNITAI_ATTACK)
+
+	# king and queen
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["QUEEN"], 5, 1, UnitAITypes.UNITAI_ATTACK)
+	gc.getPlayer(iActivePlayer).initUnit(PIECE_SETS[pieceSet]["KING"], 4, 1, UnitAITypes.UNITAI_ATTACK)
+
+
 def areUnitsEqual(unitA, unitB):
 	""" Returns True if the two provided units represent the same unit in gameplay
 
@@ -57,6 +120,8 @@ class CvCiv4ChessEventManager(CvEventManager.CvEventManager):
 		# display welcome message
 		self.parent.onGameStart(self, argsList)
 
+		setupPieces()
+
 		CyInterface().addImmediateMessage("Let's play chess", "")
 
 		# give all units the ability to view the entire board			
@@ -79,10 +144,23 @@ class CvCiv4ChessEventManager(CvEventManager.CvEventManager):
 		'Combat Result'
 		self.parent.onCombatResult(self, argsList)
 
+		CyInterface().addImmediateMessage("Combat occurred", "")
+		sys.stdout.write("fight\n")
+
 		# disable gaining experience
-		defender, attacker = argsList
+		attacker, defender = argsList
 		defender.setExperience(0, 0)
 		attacker.setExperience(0, 0)
+
+		defender.setLevel(0)
+		attacker.setLevel(0)
+
+		defender.setPromotionReady(False)
+		attacker.setPromotionReady(False)
+
+		# end attacker's movement after combat
+		attacker.finishMoves()
+		attacker.rotateFacingDirectionClockwise()
 		
 	def onUnitCreated(self, argsList):
 		'Unit Completed'
@@ -125,8 +203,6 @@ class CvCiv4ChessEventManager(CvEventManager.CvEventManager):
 	def onSetPlayerAlive(self, argsList):
 		'Set Player Alive Event'
 		self.parent.onSetPlayerAlive(self, argsList)
-		iPlayerID = argsList[0]
-		bNewValue = argsList[1]
 
 	def onVictory(self, argsList):
 		'Victory'
